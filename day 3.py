c3f40f56dd9e42a724fda052bcae09873611e9cf
adambombz <39539852+adambombz@users.noreply.github.com>
2024-12-03 02:18:53 -0500

    Update day 3.py

--- day 3.py
+++ day 3.py
@@ -1,8 +1,6 @@
 g=open("input3.txt").read()
 
-pt1=0
-pt2=0
-
+pt1,pt2=0,0
 on=True
 
 for i in range(len(g)):
@@ -13,12 +11,10 @@ for i in range(len(g)):
     if g[i:].startswith("mul("):
         mul=g[i:i+12]
         value = mul.split("mul(")[1].split(")")[0]
-        try:
+        if value.find(",")!=-1:
             a, b = value.split(",")
             if a.isdigit() and b.isdigit():
                 pt1 += int(a) * int(b)
                 if on: pt2 += int(a) * int(b)
-        except ValueError:
-            pass
 
 print("Part 1:", pt1, "Part 2:", pt2)
