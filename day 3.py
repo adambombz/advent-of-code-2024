02dc8051294e6489755f130f370a75a777f07cd8
adambombz <39539852+adambombz@users.noreply.github.com>
2024-12-03 02:15:57 -0500

    Update day 3.py

--- day 3.py
+++ day 3.py
@@ -1,40 +1,24 @@
-f=open("input3.txt").read()
-g=str(f)
+g=open("input3.txt").read()
 
 pt1=0
 pt2=0
-
-print("f".find("mul("))
-
-values = [part.split(")")[0] for part in f.split("mul(")[1:]]
-
-f=f.split("mul(")
-
-for value in values:
-    try:
-        a,b=value.split(",")
-        if a.isdigit() and b.isdigit():
-            pt1+=int(a)*int(b)
-    except ValueError:
-        pass
-
-print(pt1)
 
 on=True
 
 for i in range(len(g)):
     if g[i:].startswith("do()"):
         on=True
-    elif g[i:].startswith("don't()"):
+    if g[i:].startswith("don't()"):
         on=False
-    elif g[i:].startswith("mul(") and on:
+    if g[i:].startswith("mul("):
         mul=g[i:i+12]
         value = mul.split("mul(")[1].split(")")[0]
         try:
             a, b = value.split(",")
             if a.isdigit() and b.isdigit():
-                pt2 += int(a) * int(b)
+                pt1 += int(a) * int(b)
+                if on: pt2 += int(a) * int(b)
         except ValueError:
             pass
 
-print(pt2)
+print("Part 1:", pt1, "Part 2:", pt2)
