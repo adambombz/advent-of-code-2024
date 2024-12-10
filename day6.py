90136afcb5ee358ae6e2decccdaef136fea7d7be
adambombz <39539852+adambombz@users.noreply.github.com>
2024-12-09 21:53:10 -0500

    Update day6.py

--- day6.py
+++ day6.py
@@ -11,7 +11,7 @@ defaultDir=[-1,0]
 steps=0
 passed=set()
 
-def step(input,dir,pos):
+def step(input,dir,pos):        #step in input for direction and position, return pos with new dir if hitting # otherwise step normally and keep direction (ignore other returns)
     x=pos[0]+dir[0]
     y=pos[1]+dir[1]
     if input[x][y]=='#':
@@ -25,13 +25,13 @@ def step(input,dir,pos):
             return pos, [1,0], True, [x,y]
     return [x,y], dir, False, [x,y]
 
-def isloop(input, pos, dir):
+def isloop(input, pos, dir):            #find loops
     steps=set()
     # print(input)
-    while pos[0] >= 0 and pos[0] < len(input) - 1 and pos[1] >= 0 and pos[1] < len(input[0]) - 1:
+    while pos[0] >= 0 and pos[0] < len(input) - 1 and pos[1] >= 0 and pos[1] < len(input[0]) - 1:        #condition for position to exit the space aka no loop
         pos, dir, turned, block = step(input, dir, pos)
         # print(steps, pos, dir)
-        if tuple(pos+dir) not in steps:
+        if tuple(pos+dir) not in steps:            #pos+dir to find repeated position and direction indicating a loop
             steps.add(tuple(pos+dir))
         elif tuple(pos+dir) in steps:
             # print("returned true")
@@ -39,24 +39,24 @@ def isloop(input, pos, dir):
     # print("returned false")
     return False
 
-for i in range(len(f)):
+for i in range(len(f)):            #find start position
     for j in range(len(f)):
         if f[i][j]=="^":
             pos=[i,j]
 
-defaultPos=list(pos)
+defaultPos=list(pos)                #pt1 solution changes position so set default position for pt2
 
 print(len(f), len(f[0]), pos)
-passed.add(tuple(pos))
+passed.add(tuple(pos))                #passed is the positions that have been stepped over for part 1
 
-while pos[0]>=0 and pos[0]<len(f)-1 and pos[1]>=0 and pos[1]<len(f[0])-1:
+while pos[0]>=0 and pos[0]<len(f)-1 and pos[1]>=0 and pos[1]<len(f[0])-1:        #pt1 solve
     pos,dir, _, _= step(f,dir,pos)
     passed.add(tuple(pos))
 
 print("part1:", len(passed))
 
 inputs=[]
-for i in range(len(f)):
+for i in range(len(f)):            #create new set of inputs adding a new # in each applicable location
     for j in range(len(f[i])):
         if [i,j] != defaultPos:
             tmpIn=list(f)
@@ -65,7 +65,7 @@ for i in range(len(f)):
 
 pt2=0
 
-for input in inputs:
+for input in inputs:            #pt2 solve
     # print(input, defaultPos, defaultDir)
     if isloop(input, defaultPos, defaultDir):
         pt2+=1
